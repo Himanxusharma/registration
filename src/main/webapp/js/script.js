71ca22faabce2b01d490fd802904526eb0733def
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-04-03 18:09:59 +0530

    update js

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,10 @@
+/*var element;
+
+element = document.getElementById("msg").innerText;
+console.log(element);
+*/
+document.getElementById("msg").setAttribute("class","xyz");
+/*document.getElementById("msg").innerHTML = "<H1>Hola, </H1>";
+element = document.getElementById("msg").innerHTML;*//*
+
+console.log(element);*/
